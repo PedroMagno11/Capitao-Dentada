f1b06f0691ab008ec81726c04b3ffbb61ad539b9
beaps_ <beatriz01ps@gmail.com>
2026-08-04 11:57:20 -0300

    Adaptação para geração do .exe

---
+++ Scripts/navio2.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+@onready var timer: Timer = $Timer
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	body.is_in_group("player")
+	get_tree().change_scene_to_file("res://Cenas/naval_3d/travessia_03.tscn")
+	timer.start(2.0)
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
